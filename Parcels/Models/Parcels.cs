eff870eeaa54fef5392646f1f3cb901e4db5b69e
Adilet MoMUNALIEV and Misaki Koonce <misaki@>
2020-01-07 12:01:19 -0800

    Add methods to calculate shipping cost.

---
+++ Parcels/Models/Parcels.cs
@@ -0,0 +1,101 @@
+namespace ShippingParcel.Models
+{
+    public class Parcel
+    {
+        private double width;
+        private double length;
+        private double height;
+        private double weight;
+        private double price;
+
+        public Parcel (double width, double length, double height, double weight)
+        {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+            this.weight = weight;
+            this.price = 0;
+        }
+
+        public double GetWidth()
+        {
+            return this.width;
+        }
+
+        public void SetWidth(double new_width)
+        {
+            this.width = new_width;
+        }
+
+        public double GetLength()
+        {
+            return this.length;
+        }
+
+        public void SetLength(double new_length)
+        {
+            this.length = new_length;
+        }
+
+        public double GetHeight()
+        {
+            return this.height;
+        }
+
+        public void SetHeight(double new_height)
+        {
+            this.height = new_height;
+        }
+
+        public double GetWeight()
+        {
+            return this.weight;
+        }
+
+        public void SetWeight(double new_weight)
+        {
+            this.weight = new_weight;
+        }
+
+        public string CheckValidation()
+        {
+            if (this.width < 0 || this.length < 0 || this.height < 0 || this.weight < 0)
+            {
+                return "Invalid input.";
+            }
+            else if (this.width == null || this.length == null || this.height == null || this.weight == null)
+            {
+                return "Fill all the fields.";
+            }
+            return "Valid input!";
+        }
+
+        public double Volume()
+        {
+            return this.width * this.length * this.height;
+        }
+
+        public double CostToShip()
+        {
+            double dimensions = Volume();
+            if (dimensions < 30)
+            {
+                this.price = 50;
+            }
+            else if (dimensions >= 30 && dimensions < 60)
+            {
+                this.price = 100;
+            }
+            else
+            {
+                this.price = 150;
+            }
+            return this.price;
+        }
+
+        // public string ToString()
+        // {
+
+        // }
+    }
+}
